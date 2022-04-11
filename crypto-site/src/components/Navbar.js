e87c8716d9d0c3174813eb26ceb9b61f9b709cb3
Vince Salapuddin <vsalapuddin@ufl.edu>
2022-04-11 02:38:03 -0400

    Group 32 fix

--- crypto-site/src/components/Navbar.js
+++ crypto-site/src/components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar= () =>{
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
     <a className="navbar-brand" href="#">
-      Group 
+      Group 32
     </a>
     <button
       className="navbar-toggler"
