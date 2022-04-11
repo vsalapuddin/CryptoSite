333cad6d90e73f5ebf7b737fda44a0667d8fc712
Vince Salapuddin <vsalapuddin@ufl.edu>
2022-04-11 02:36:54 -0400

    setup basic routing and template

---
+++ crypto-site/src/components/Navbar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {  Link } from "react-router-dom";
+const Navbar= () =>{
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <a className="navbar-brand" href="#">
+      Group 32
+    </a>
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNavDropdown"
+      aria-controls="navbarNavDropdown"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul className="navbar-nav">
+        <li className="nav-item active">
+          <a className="nav-link" href="/Home">
+            Home 
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="Charts">
+            Charts
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="Data">
+            Data
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  );
+}
+export default Navbar;
